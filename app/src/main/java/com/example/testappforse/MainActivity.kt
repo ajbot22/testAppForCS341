cf8719e5843d42da05d23d2ddec0680d3e12ee3d
ajbot22 <ajbot2002@gmail.com>
2023-09-21 09:36:11 -0400

    First Commit

---
+++ app/src/main/java/com/example/testappforse/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.testappforse
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.testappforse.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TODO: (ADD) Check a shared preferences for a list of added items and quantities
+        //TODO: (ADD) Populate the screen with the list of items
+        //TODO: (REMOVE) Either add a swipe to delete functionality or some kind of multi-select to delete
+        //TODO: (CHECKOUT) Connect to the database to checkout the items
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                //TODO: Rename and change the images for these buttons to be add,remove,checkout
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
