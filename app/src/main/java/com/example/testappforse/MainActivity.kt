97cd30e7b2ab34befc00715565cc9623e3425fed
ajbot22 <ajbot2002@gmail.com>
2023-09-24 16:11:31 -0400

    First Commit on new branch, Created functions to read and parse the Json data from the url (have not yet done the code to show it)

--- app/src/main/java/com/example/testappforse/MainActivity.kt
+++ app/src/main/java/com/example/testappforse/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.testappforse
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.testappforse.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +19,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*  A test data sampler if we cannot get the url working for some reason
+            In that case, skip the retrieveJson func and just use parse with this data
+
+        val jsonData = """{
+        "productID": "12345",
+        "productName": "Example Product",
+        "quantity": "10",
+        "catID": "6789",
+        "img": "example.jpg"
+         }"""
+         */
+
+        val jsonStr = retrieveJsonFromUrl()
+        val data = parseJsonToDataModel(jsonStr)
         //TODO: (ADD) Check a shared preferences for a list of added items and quantities
         //TODO: (ADD) Populate the screen with the list of items
         //TODO: (REMOVE) Either add a swipe to delete functionality or some kind of multi-select to delete
+        //TODO: (REMOVE) Create a Recyler View for all the data objects gained from Add
         //TODO: (CHECKOUT) Connect to the database to checkout the items
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -36,3 +55,25 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 }
+
+data class DataModel(
+    val productID: String,
+    val productName: String,
+    val quantity: String,
+    val catID: String,
+    val img: String
+)
+fun retrieveJsonFromUrl(): String {
+    val url = URL("https://bluejaypantry.etowndb.com/data_src/data.php?table=products")
+    val connection = url.openConnection() as HttpURLConnection
+    connection.requestMethod = "GET"
+    connection.connect()
+
+    return connection.inputStream.bufferedReader().use { it.readText() }
+}
+
+fun parseJsonToDataModel(jsonData: String): DataModel {
+    val gson = Gson()
+    return gson.fromJson(jsonData, DataModel::class.java)
+}
+
